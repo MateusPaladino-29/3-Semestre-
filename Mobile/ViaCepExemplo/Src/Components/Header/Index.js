d5e624221f8b9984b7a1f1bf89a588aed1e46aec
Paladino <Teixeirapaladino921@gmail.com>
2024-01-31 17:17:43 -0300

    Iniciamos o projeto Via Cep

---
+++ Mobile/ViaCepExemplo/Src/Components/Header/Index.js
@@ -0,0 +1,9 @@
+import { HeaderContainer, HeaderContent, TextHeader } from "./Styles";
+
+export function Header() {
+    <HeaderContainer>
+            <HeaderContent>
+                <TextHeader>Consumo da API Via Cep</TextHeader>
+            </HeaderContent>
+    </HeaderContainer>
+}
---
+++ Mobile/ViaCepExemplo/Src/Components/Header/Styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.View`
+background-color: #FECC2B;
+border-radius: 30px;
+height: 20px;
+justify-content: center;
+align-items:center;
+`
+export const HeaderContent = styled.SafeAreaView`
+
+`
+
+export const TextHeader = styled.Text`
+font-size: 24px;
+font-family: Roboto_500Medium;
+color: #333E33;
+`
+
